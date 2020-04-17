9ee1bd2daf6cd1d8a938619444033a0bb52a7716
William Rocha <william.rocha@lg.com.br>
2020-04-16 21:18:33 -0300

    Adiciona v1 do Swagger

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerDocument = require('../app/swagger.json');
  
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
