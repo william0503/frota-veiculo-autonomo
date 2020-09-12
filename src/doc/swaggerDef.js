286b4b8d1a0429f517bd7913b212dcc6f75fafa6
William Rocha <william0503@gmail.com>
2020-09-11 21:04:26 -0300

    ajuste swagger

--- src/doc/swaggerDef.js
+++ src/doc/swaggerDef.js
@@ -2,30 +2,29 @@ require('dotenv/config');
 const swaggerJsdoc = require('swagger-jsdoc');
 
 const options = {
-    swaggerDefinition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Frota de veículos autonomos",
-        version: "1.0.0",
-        description:  "API para gestão de frota de veículos autonomos",
-        license: {
-          name: "MIT",
-          url: "https://choosealicense.com/licenses/mit/"
-        },
-        contact: {
-          name: "Swagger",
-          url: "https://swagger.io",
-          email: "Info@SmartBear.com"
-        }
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Frota de veículos autonomos',
+      version: '1.0.0',
+      description: 'API para gestão de frota de veículos autonomos',
+      license: {
+        name: 'MIT',
+        url: 'https://choosealicense.com/licenses/mit/',
       },
-      servers: [
-        {
-          url: `${process.env.HOST}:${process.env.PORT}/api/`
-        }
-      ]
+      contact: {
+        name: 'Swagger',
+        url: 'https://swagger.io',
+        email: 'Info@SmartBear.com',
+      },
     },
-    apis: ['./src/routes.js', './src/models/*.js']
-  };
-
+    servers: [
+      {
+        url: `${process.env.HOST}/api/`,
+      },
+    ],
+  },
+  apis: ['./src/routes.js', './src/models/*.js'],
+};
 
 module.exports = swaggerJsdoc(options);
